464680411dd445796a63f2d881c22fc26267395d
Abdullahinur <aabdullahinur@hotmail.com>
2017-12-05 20:14:59 +0300

    add function to call informatiom from api add css styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Http, Response } from '@angular/http'
+import { gitHubDetails} from './lookup-model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    gitName = '';
+    gitHubDetails;
+    name = '';
+    id = '';
+    url = '';
+    avatar_url = '';
+
+  constructor (private http:Http) {}
+
+    searchGitName() {
+      this.http.get('https://api.github.com/users/' +this.gitName).subscribe(
+        (res:Response)=>{
+          const gitHubDetails1 =res.json();
+          console.log(gitHubDetails1)
+          this.gitHubDetails= gitHubDetails1;
+
+
+        }
+      )
+
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http'
 
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
+
   ],
-  providers: [],
+  providers: [HttpModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
